d72ea90ec9ad9eea9a2510cc030cdb99cada08c1
DHPapazian <DHPapazian23@codingburgas.bg>
2026-01-29 14:57:27 +0200

    Add all functions

---
+++ math1/Project1/math2.cpp
@@ -0,0 +1,13 @@
+#include "../Project1/math2.h"
+int add1(int a, int b) {
+	return a + b;
+}
+int subtract(int a, int b) {
+	return a - b;
+}
+int multiply(int a, int b) {
+	return a * b;
+}
+int divide(int a, int b) {
+	return a / b;
+}
